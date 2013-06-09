d83d78821948f9644f2d60e40b93289bcbd04ff2
simcap <caplette@hotmail.co.uk>
2013-06-09 10:48:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wipgit::Application.config.secret_token = '2942803149b75dc6ac6df260aa0a5cafcbb8ccbe987027f621e59d789d94debcd8476487a97861b7b5b1b411bd8d54be6e84f8f086b43a7f50521c048bf17c7e'
